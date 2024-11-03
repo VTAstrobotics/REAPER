54743f25a7ea97ff0223933539cffbfa442ea697
camtriggerhappy <60159291+camtriggerhappy@users.noreply.github.com>
2024-11-03 17:58:18 -0500

    Update can_startup.sh

--- launch_scripts/can_startup.sh
+++ launch_scripts/can_startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #from https://canable.io/getting-started.html bc we have a knock off canable
-    sudo slcand -o -c -s8 /dev/CAN-SERIAL-LINK can0 # starts interface at 1MBPS
     sudo ifconfig can0 up
     sudo ifconfig can0 txqueuelen 1000
 echo "CAN interface started, haul some rocks"
