064c03759e1030332ed65bac433c3337451c59ba
Brett Atwood <bretta45@vt.edu>
2025-02-08 01:24:34 +0000

    updated client code

--- src/teliop_control/src/client.cpp
+++ src/teliop_control/src/client.cpp
@@ -2,17 +2,14 @@
 #include <memory>
 #include <string>
 #include <sstream>
-
-// #include "sensor_msgs/msg/dig.hpp"
-// #include "sensor_msgs/msg/drivetrain.hpp"
+#include <functional>
 #include "action_interfaces/action/dump.hpp"
+#include "action_interfaces/action/dig.hpp"
+#include "action_interfaces/action/drive.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "rclcpp/client.hpp"
-//#include "rclcpp/client_goal_handle.hpp"
-//#include "rclcpp/client_goal_handle_impl.hpp"
-//#include "rclcpp_action.hpp"
 #include "settings.h"
 #include "utils.h"
 
@@ -26,33 +23,31 @@ class Distributor : public rclcpp::Node
 public:
     using Dump = action_interfaces::action::Dump;
     using DumpGoalHandle = rclcpp_action::ClientGoalHandle<Dump>;
+    using Dig = action_interfaces::action::Dig;
+    using DigGoalHandle = rclcpp_action::ClientGoalHandle<Dig>;
+    using Drive = action_interfaces::action::Drive;
+    using DriveGoalHandle = rclcpp_action::ClientGoalHandle<Drive>;
 
 
     explicit Distributor(const rclcpp::NodeOptions &options)
         : Node("distrib", options)
     {
         this->Dump_ptr_ = rclcpp_action::create_client<action_interfaces::action::Dump>(this, "Dump");
+        this->Dig_ptr_ = rclcpp_action::create_client<action_interfaces::action::Dig>(this, "Dig");
+        this->Drive_ptr_ = rclcpp_action::create_client<action_interfaces::action::Drive>(this, "Drive");
 
         Joy_subscription = this->create_subscription<sensor_msgs::msg::Joy>("/joy", 2, std::bind(&Distributor::topic_callback, this, _1));
 
-        auto goal_msg = Dump::Goal();
-
-        digMode = false;
-        cooldown = false;
-        last_time = 0;
     }
 
     Distributor() : Node("distrib")
     {
         this->Dump_ptr_ = rclcpp_action::create_client<action_interfaces::action::Dump>(this, "Dump");
+        this->Dig_ptr_ = rclcpp_action::create_client<action_interfaces::action::Dig>(this, "Dig");
+        this->Drive_ptr_ = rclcpp_action::create_client<action_interfaces::action::Drive>(this, "Drive");
 
         Joy_subscription = this->create_subscription<sensor_msgs::msg::Joy>("/joy", 2, std::bind(&Distributor::topic_callback, this, _1));
 
-        auto goal_msg = Dump::Goal();
-
-        digMode = false;
-        cooldown = false;
-        last_time = 0;
     }
 private:
     
@@ -61,26 +56,58 @@ private:
     */
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr Joy_subscription;
     rclcpp_action::Client<action_interfaces::action::Dump>::SharedPtr Dump_ptr_;
-    //rclcpp_action::Client<action_interfaces::action::Dig>::SharedPtr Dig_ptr_;
-    //rclcpp_action::Client<action_interfaces::action::Drive>::SharedPtr Drive_ptr_;
+    rclcpp_action::Client<action_interfaces::action::Dig>::SharedPtr Dig_ptr_;
+    rclcpp_action::Client<action_interfaces::action::Drive>::SharedPtr Drive_ptr_;
 
     /*
     ?????Joystick messages?????
     */
     void topic_callback(const sensor_msgs::msg::Joy &distribRaw)
     {
+        auto Dump_goal_msg = Dump::Goal();
+        auto Dig_goal_msg = Dig::Goal();
+        auto Drive_goal_msg = Drive::Goal();
+        
+        // Dump controls
+        if (distribRaw.buttons[BUTTON_Y]){
+            auto send_dump_goal_options = rclcpp_action::Client<Dump>::SendGoalOptions();
+            send_dump_goal_options.goal_response_callback = std::bind(&Distributor::dumpResponseCallback, this , _1);
 
+            //send_dump_goal_options.feedback_callback = std::bind(&Distributor::dumpFeedbackCallback, this, _1, _2);
+            //send_dump_goal_options.result_callback = std::bind(&Distributor::dumpResultCallback, this, _1);
+            this->Dump_ptr_->async_send_goal(Dump_goal_msg, send_dump_goal_options);
+        }
+        /*
+        if (distribRaw.buttons[BUTTON_A]){
+            auto send_dump_goal_options = rclcpp_action::Client<Dump>::SendGoalOptions();
+            send_dump_goal_options.goal_response_callback = std::bind(&Distributor::dumpResponseCallback, this, _2);
+            //send_dump_goal_options.feedback_callback = std::bind(&Distributor::dumpFeedbackCallback, this, _1, _2);
+            //send_dump_goal_options.result_callback = std::bind(&Distributor::dumpResultCallback, this, _1);
+            this->Dump_ptr_->async_send_goal(Dump_goal_msg, send_dump_goal_options);
+        }
+       */
+       
+        /*
+        // Dig controls
+        if ()
+        {
+
+        }
+        // Drive controls
+        if ()
+        {
+
+        }
+        */
     }
-    bool cooldown;
-    bool digMode;
-    int32_t last_time;
-    
 
     /*
     Sending Goals (dump, dig, drive)
     */
     void goal_msgs(){
         auto dumpSend_goal = rclcpp_action::Client<action_interfaces::action::Dump>::SendGoalOptions();
+        auto digSend_goal = rclcpp_action::Client<action_interfaces::action::Dig>::SendGoalOptions();
+        auto driveSend_goal = rclcpp_action::Client<action_interfaces::action::Drive>::SendGoalOptions();
 
     }
 
@@ -89,14 +116,14 @@ private:
      */
     void dumpResponseCallback(std::shared_future<DumpGoalHandle::SharedPtr> future)
     {
-        auto goal_handle = future.get();
-        if (!goal_handle){
+        auto extracted_goal_handle = DumpGoalHandle.get();
+        if (!extracted_goal_handle){
             RCLCPP_ERROR(this->get_logger(), "Dump goal was rejected by server");
         } else {
-            RCLCPP_INFO(this->get_logger(), "Dummp goal accepted by server, waiting for result");
+            RCLCPP_INFO(this->get_logger(), "Dump goal accepted by server, waiting for result");
         }
-    }
-    /*
+    }    
+/*
     void digResponseCallback(std::shared_future<DigGoalHandle::SharedPtr> future)
     {
         auto goal_handle = future.get();
@@ -107,7 +134,7 @@ private:
         }    
     }
 
-    void driveResponseCallback(std::shared_future<DriveGoalHandle::SharedPtr> future)
+    void driveResponseCallback(std::shared_ptr<rclcpp_action::ClientGoalHandle<action_interfaces::action::Dump>> goal_handle)
     {
         auto goal_handle = future.get();
         if (!goal_handle){
@@ -116,8 +143,7 @@ private:
             RCLCPP_INFO(this->get_logger(), "Drive goal accepted by server, waiting for result");
         }    
     }
-    */
-
+*/
 };
 
 int main(int argc, char *argv[])
