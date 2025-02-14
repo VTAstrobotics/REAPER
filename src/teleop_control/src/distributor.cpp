058b01f332fda5aa9162c1e60429b2bad657098b
Henry Mazaika <henrymazaika@gmail.com>
2025-02-14 23:18:22 +0000

    added infrastructure for dig and drive. couldnt test because those action servers are not working, so im committing before i merge updated code to try to fix them so i can test

--- src/teleop_control/src/distributor.cpp
+++ src/teleop_control/src/distributor.cpp
@@ -18,6 +18,7 @@
 #include "utils.h"
 
 using std::placeholders::_1;
+using std::placeholders::_2;
 
 namespace teleop_control
 {
@@ -66,47 +67,60 @@ private:
     */
     void joy1_cb(const sensor_msgs::msg::Joy &raw)
     {
-        auto dump_goal = Dump::Goal();
-        dump_goal.deposition_goal = 0.01;
-        // auto dig_goal = Dig::Goal();
-        // auto drive_goal = Drive::Goal();
+        // Drive controls
+        auto drive_goal = Drive::Goal();
+        geometry_msgs::msg::Twist drive_vel;
+        drive_vel.linear.x = 1;
+        drive_vel.linear.y = 2;
+        drive_vel.linear.z = 3;
+        drive_vel.angular.x = 4;
+        drive_vel.angular.y = 5;
+        drive_vel.angular.z = 6;
+
+        // Dig controls
+        auto dig_goal = Dig::Goal();
+        dig_goal.auton = true;
 
         // Dump controls
-        if (raw.buttons[BUTTON_Y]){
-            auto send_dump_goal_options = rclcpp_action::Client<Dump>::SendGoalOptions();
-            send_dump_goal_options.goal_response_callback = std::bind(&Distributor::dump_cb, this , _1);
+        auto dump_goal = Dump::Goal();
+        dump_goal.deposition_goal = 0.01;
 
-            // send_dump_goal_options.feedback_callback = std::bind(&Distributor::dumpFeedbackCallback, this, _1, _2);
-            // send_dump_goal_options.result_callback = std::bind(&Distributor::dumpResultCallback, this, _1);
+        if (raw.buttons[BUTTON_A]){ //
+            auto send_dump_goal_options = rclcpp_action::Client<Dump>::SendGoalOptions();
+            send_dump_goal_options.goal_response_callback = std::bind(&Distributor::dump_response_cb, this, _1);
+            send_dump_goal_options.feedback_callback = std::bind(&Distributor::dump_fb_cb, this, _1, _2);
+            send_dump_goal_options.result_callback = std::bind(&Distributor::dump_result_cb, this, _1);
             this->dump_ptr_->async_send_goal(dump_goal, send_dump_goal_options);
         }
 
-        /*
-        if (raw.buttons[BUTTON_A]){
-            auto send_dump_goal_options = rclcpp_action::Client<Dump>::SendGoalOptions();
-            send_dump_goal_options.goal_response_callback = std::bind(&Distributor::dump_cb, this, _2);
-            //send_dump_goal_options.feedback_callback = std::bind(&Distributor::dumpFeedbackCallback, this, _1, _2);
-            //send_dump_goal_options.result_callback = std::bind(&Distributor::dumpResultCallback, this, _1);
-            this->dump_ptr_->async_send_goal(dump_goal, send_dump_goal_options);
+        if (raw.buttons[BUTTON_B]){ //
+            auto send_dig_goal_options = rclcpp_action::Client<Dig>::SendGoalOptions();
+            send_dig_goal_options.goal_response_callback = std::bind(&Distributor::dig_response_cb, this, _1);
+            send_dig_goal_options.feedback_callback = std::bind(&Distributor::dig_fb_cb, this, _1, _2);
+            send_dig_goal_options.result_callback = std::bind(&Distributor::dig_result_cb, this, _1);
+            this->dig_ptr_->async_send_goal(dig_goal, send_dig_goal_options);
         }
-       */
 
-        /*
-        // Dig controls
-        if ()
-        {
-
+        if (raw.buttons[BUTTON_X]){ //
+            auto send_drive_goal_options = rclcpp_action::Client<Drive>::SendGoalOptions();
+            send_drive_goal_options.goal_response_callback = std::bind(&Distributor::drive_response_cb, this, _1);
+            send_drive_goal_options.feedback_callback = std::bind(&Distributor::drive_fb_cb, this, _1, _2);
+            send_drive_goal_options.result_callback = std::bind(&Distributor::drive_result_cb, this, _1);
+            this->drive_ptr_->async_send_goal(drive_goal, send_drive_goal_options);
         }
-        // Drive controls
-        if ()
-        {
 
-        }
-        */
+        // if (raw.buttons[BUTTON_Y]){ //
+        //     auto send_dump_goal_options = rclcpp_action::Client<Dump>::SendGoalOptions();
+        //     send_dump_goal_options.goal_response_callback = std::bind(&Distributor::dump_response_cb, this, _1);
+        //     send_dump_goal_options.feedback_callback = std::bind(&Distributor::dump_fb_cb, this, _1, _2);
+        //     send_dump_goal_options.result_callback = std::bind(&Distributor::dump_result_cb, this, _1);
+        //     this->dump_ptr_->async_send_goal(dump_goal, send_dump_goal_options);
+        // }
+
     }
 
-    /*
-    Sending Goals (dump, dig, drive)
+    /**
+     * Sending Goals (dump, dig, drive)
     */
     void goal_msgs(){
         auto dumpSend_goal = rclcpp_action::Client<Dump>::SendGoalOptions();
@@ -115,37 +129,129 @@ private:
 
     }
 
-    /*
-    Call backs (dump, dig, drive)
-     */
-    void dump_cb(const DumpGoalHandle::SharedPtr& goal_handle)
+    void drive_response_cb(const DriveGoalHandle::SharedPtr& goal_handle)
     {
-        if (!goal_handle){
-            RCLCPP_ERROR(this->get_logger(), "Dump goal was rejected by server");
-        } else {
-            RCLCPP_INFO(this->get_logger(), "Dump goal accepted by server, waiting for result");
-        }
-    }
-
-/*
-    void dig_cb(const DigGoalHandle::SharedPtr& goal_handle)
-    {
-        if (!goal_handle){
-            RCLCPP_ERROR(this->get_logger(), "Dig goal was rejected by server");
-        } else {
-            RCLCPP_INFO(this->get_logger(), "Dig goal accepted by server, waiting for result");
-        }
-    }
-
-    void drive_cb(const DriveGoalHandle::SharedPtr& goal_handle)
-    {
-        if (!goal_handle){
-            RCLCPP_ERROR(this->get_logger(), "Drive goal was rejected by server");
-        } else {
+        if (goal_handle) {
             RCLCPP_INFO(this->get_logger(), "Drive goal accepted by server, waiting for result");
+        } else {
+            RCLCPP_ERROR(this->get_logger(), "Drive goal was rejected by server");
         }
     }
-*/
+
+    /**
+     * @param goal_handle
+     */
+    void drive_fb_cb(DriveGoalHandle::SharedPtr, const std::shared_ptr<const Drive::Feedback> feedback)
+    {
+        RCLCPP_INFO(this->get_logger(), "Drive instantaneous velocity:\nlinear\n\tx = %f\n\ty = %f\n\tz = %f\nangular\n\tx = %f\n\ty = %f\n\tz = %f",
+        feedback->inst_velocity.linear.x, feedback->inst_velocity.linear.y, feedback->inst_velocity.linear.z,
+        feedback->inst_velocity.angular.x, feedback->inst_velocity.angular.y, feedback->inst_velocity.angular.z);
+    }
+
+    /**
+     * @param result
+     */
+    void drive_result_cb(const DriveGoalHandle::WrappedResult& result)
+    {
+        switch (result.code) {
+        case rclcpp_action::ResultCode::SUCCEEDED:
+            break;
+        case rclcpp_action::ResultCode::ABORTED:
+            RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+            return;
+        case rclcpp_action::ResultCode::CANCELED:
+            RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+            return;
+        default:
+            RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+            return;
+        }
+
+        RCLCPP_INFO(this->get_logger(), "Drive velocity:\nlinear\n\tx = %f\n\ty = %f\n\tz = %f\nangular\n\tx = %f\n\ty = %f\n\tz = %f",
+        result.result->curr_velocity.linear.x, result.result->curr_velocity.linear.y, result.result->curr_velocity.linear.z,
+        result.result->curr_velocity.angular.x, result.result->curr_velocity.angular.y, result.result->curr_velocity.angular.z);
+    }
+
+    void dig_response_cb(const DigGoalHandle::SharedPtr& goal_handle)
+    {
+        if (goal_handle) {
+            RCLCPP_INFO(this->get_logger(), "Dig goal accepted by server, waiting for result");
+        } else {
+            RCLCPP_ERROR(this->get_logger(), "Dig goal was rejected by server");
+        }
+    }
+
+    /**
+     * @param goal_handle
+     */
+    void dig_fb_cb(DigGoalHandle::SharedPtr, const std::shared_ptr<const Dig::Feedback> feedback)
+    {
+        RCLCPP_INFO(this->get_logger(), "Dig linkage %f%% completed and bucket %f%% completed", feedback->percent_link_done, feedback->percent_bckt_done);
+    }
+
+    /**
+     * @param result
+     */
+    void dig_result_cb(const DigGoalHandle::WrappedResult& result)
+    {
+        switch (result.code) {
+        case rclcpp_action::ResultCode::SUCCEEDED:
+            break;
+        case rclcpp_action::ResultCode::ABORTED:
+            RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+            return;
+        case rclcpp_action::ResultCode::CANCELED:
+            RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+            return;
+        default:
+            RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+            return;
+        }
+
+        RCLCPP_INFO(this->get_logger(), "Dig linkage at %f and bucket at %f (estimated)", result.result->est_dig_link_goal, result.result->est_dig_bckt_goal);
+    }
+
+    /**
+     * @param goal_handle
+     */
+    void dump_response_cb(const DumpGoalHandle::SharedPtr& goal_handle)
+    {
+        if (goal_handle) {
+            RCLCPP_INFO(this->get_logger(), "Dump goal accepted by server, waiting for result");
+        } else {
+            RCLCPP_ERROR(this->get_logger(), "Dump goal was rejected by server");
+        }
+    }
+
+    /**
+     * @param goal_handle
+     */
+    void dump_fb_cb(DumpGoalHandle::SharedPtr, const std::shared_ptr<const Dump::Feedback> feedback)
+    {
+        RCLCPP_INFO(this->get_logger(), "Dump action %f%% completed", feedback->percent_done);
+    }
+
+    /**
+     * @param result
+     */
+    void dump_result_cb(const DumpGoalHandle::WrappedResult& result)
+    {
+        switch (result.code) {
+        case rclcpp_action::ResultCode::SUCCEEDED:
+            break;
+        case rclcpp_action::ResultCode::ABORTED:
+            RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+            return;
+        case rclcpp_action::ResultCode::CANCELED:
+            RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+            return;
+        default:
+            RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+            return;
+        }
+
+        RCLCPP_INFO(this->get_logger(), "Dump deposited %f m^3 (estimation)", result.result->est_deposit_goal);
+    }
 
 }; // class Distributor
 
