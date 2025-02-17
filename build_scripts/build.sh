2752021473e3aaedda781539906e9d81d6daf391
Henry Mazaika <henrymazaika@gmail.com>
2025-02-17 18:52:47 +0000

    fixed build script

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-printf "\nbuild.sh:\n"
-echo "Changing directory"
-cd /workspaces/REAPER/src/
+echo build.sh:
 
-echo "Building project"
-colcon build --symlink-install
+# Check if the script is being sourced or run directly
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    echo "build.sh: [ERROR]: This script must be sourced, not executed."
+    echo "build.sh: [INFO]: Usage: source build.sh"
+    exit 1
+fi
 
-echo "Sourcing"
+cd /workspaces/REAPER
+
+echo build.sh: building
+
+colcon build --packages-select action_interfaces
 source install/setup.bash
 
-printf "build.sh done.\n\n"
+colcon build
+source install/setup.bash
+
+echo build.sh: done
