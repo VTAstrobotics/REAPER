1acdae37c72b580f034ea97e5cf12893fe529958
Henry Mazaika <henrymazaika@gmail.com>
2025-02-19 05:21:49 +0000

    should work now? (finally)

--- .github/actions/cppformat/entrypoint.sh
+++ .github/actions/cppformat/entrypoint.sh
@@ -30,10 +30,10 @@ echo "## Running clang-format on C/C++ source"
 SRC=$(git ls-tree --full-tree -r HEAD | grep -e "\.\(c\|h\|hpp\|cpp\)\$" | cut -f 2)
 
 # for clang-tidy
-# colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 clang-format -style=file -i $SRC
-# clang-tidy src/dump/src/dump_server.cpp --config-file=.clang-tidy -p /workspaces/REAPER/build/ --fix-errors
+clang-tidy src/dump/src/dump_server.cpp --config-file=.clang-tidy -p /workspaces/REAPER/build/ --fix-errors
 
 echo "## Commiting files..."
 git commit -am "apply clang-format" || true
