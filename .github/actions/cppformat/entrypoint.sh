6aae0ef62d6982fbccaf8895e49f1055201e9374
Henry Mazaika <henrymazaika@gmail.com>
2025-03-02 01:17:33 +0000

    no sourcing cuz it's already in bashrc

--- .github/actions/cppformat/entrypoint.sh
+++ .github/actions/cppformat/entrypoint.sh
@@ -26,20 +26,25 @@ git config --global user.name "Clang Format"
 # Ignore workflow files (we may not touch them)
 git update-index --assume-unchanged .github/workflows/*
 
-echo "## Running clang-format on C/C++ source"
+echo "## Setting up clang-format on C/C++ source"
 SRC=$(git ls-tree --full-tree -r HEAD | grep -e "\.\(c\|h\|hpp\|cpp\)\$" | cut -f 2)
 
 # for clang-tidy
-source /opt/ros/humble/setup.bash
+echo "## Setting up clang-tidy"
+echo "### build action_interfaces"
 colcon build --packages-select action_interfaces
+echo "### source reaper"
 source install/setup.bash
+echo "### build reaper"
 colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
+echo "## Running clang-format on C/C++ src code"
 clang-format -style=file -i $SRC
+echo "## Running clang-tidy on C/C++ src code"
 clang-tidy src/dump/src/dump_server.cpp --config-file=.clang-tidy -p /workspaces/REAPER/build/ --fix-errors
 
 echo "## Commiting files..."
-git commit -am "apply clang-format" || true
+git commit -am "applied C/C++ auto formatting" || true
 
 echo "## Pushing to $BRANCH"
 git push -u origin $BRANCH
