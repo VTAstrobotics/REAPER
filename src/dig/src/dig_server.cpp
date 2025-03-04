f83d3616bfae95055fb36cef7e719f8028c04d00
Henry Mazaika <henrymazaika@gmail.com>
2025-03-04 23:44:16 +0000

    fixed minor issues

--- src/dig/src/dig_server.cpp
+++ src/dig/src/dig_server.cpp
@@ -150,7 +150,7 @@ namespace dig_server
       // TODO finish this?
       // controls::PositionVoltage linkPV = controls::PositionVoltage{0_tr}.WithSlot(0);
 
-      // // Left vibration motor (neo550) configuration
+      // Left vibration motor (NEO550) configuration
       // l_vib_mtr_.SetIdleMode(IdleMode::kCoast);
       // l_vib_mtr_.SetMotorType(MotorType::kBrushless);
       // l_vib_mtr_.SetSmartCurrentFreeLimit(10.0);
@@ -163,25 +163,6 @@ namespace dig_server
       // r_vib_mtr_.SetSmartCurrentFreeLimit(10.0);
       // r_vib_mtr_.SetSmartCurrentStallLimit(10.0);
       // r_vib_mtr_.BurnFlash();
-
-      // Hardstop linear actuator configuration
-      // hstp_mtr_.SetIdleMode(IdleMode::kBrake);
-      // hstp_mtr_.SetMotorType(MotorType::kBrushed);
-      // hstp_mtr_.SetSmartCurrentFreeLimit(10.0);
-      // hstp_mtr_.SetSmartCurrentStallLimit(10.0);
-
-      // // PIDController hstp_pid(hstp_mtr_);
-      // K_u = 3.9, T_u = 0.04; // TODO: tune these values for the hardstop.
-      // hstp_pid.SetP(0, 0.8 * K_u);
-      // hstp_pid.SetI(0, 0.);
-      // hstp_pid.SetD(0, 0.1 * K_u * T_u); // 0; PD controller
-
-      // // Configure smart motion settings for velocity control
-      // hstp_pid.SetSmartMotionMaxVelocity(0, 100.);  // Max velocity in RPM
-      // hstp_pid.SetSmartMotionMaxAccel(0, 10.);     // Max acceleration in RPM/s
-
-      // hstp_mtr_.BurnFlash();
-
 
 
       RCLCPP_DEBUG(this->get_logger(), "Ready for action");
@@ -205,9 +186,6 @@ namespace dig_server
     // bucket rotators
     hardware::TalonFX l_bckt_mtr_{21, "can0"};
     hardware::TalonFX r_bckt_mtr_{24, "can0"};
-
-    // hardstop linear actuator
-    // SparkMax hstp_mtr_{"can0", 26};
     hardware::CANcoder bckt_cancoder_{1, "can0"};
     controls::PositionDutyCycle l_bckt_pos_duty_cycle_{0 * 0_tr}; // absolute position to reach (in rotations)
     mechanisms::SimpleDifferentialMechanism bckt_mech{l_bckt_mtr_, r_bckt_mtr_, true};
@@ -410,7 +388,7 @@ namespace dig_server
         pwr = 0;
       }
 
-      
+
 
 
 
@@ -438,8 +416,6 @@ namespace dig_server
         pwr = 0;
       }
 
-      // hstp_mtr_.Heartbeat();
-      // hstp_mtr_.SetDutyCycle(pwr);
       // l_vib_mtr_.Heartbeat();
       // r_vib_mtr_.Heartbeat();
 
