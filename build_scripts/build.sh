8ba53e7a4c33e409c58caa95f0e0178fa6858ecd
Astrobotics <vtastrobotics@gmail.com>
2025-03-04 21:36:50 -0500

    Revert "fixed the build dependencies. also added git mergetool"
    This reverts commit 6722c8628fcb2a9ee84fc8228c748039d64cdbc6.

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -3,14 +3,22 @@
 # Check if the script is being sourced or run directly
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     echo "build.sh: [ERROR]: This script must be sourced, not executed."
-    echo "build.sh: [INFO]: Usage: source build_scripts/build.sh"
+    echo "build.sh: [INFO]: Usage: source build.sh"
     exit 1
 fi
 
-echo build.sh: building
-colcon build --cmake-args=-DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON
+cd /workspaces/REAPER
 
-echo build.sh: sourcing
+echo build.sh: building
+
+colcon build --packages-select action_interfaces state_messages
+colcon build --packages-select state_messages_utils
+source install/setup.bash
+
+colcon build --packages-select state_messages_utils
+source install/setup.bash
+
+colcon build --cmake-args=-DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON --packages-skip action_interfaces state_messages state_messages_utils
 source install/setup.bash
 
 echo build.sh: done
