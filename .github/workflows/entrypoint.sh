546513c41f8f69e8e30146df6efb02b3e9b9d5c4
Henry Mazaika <henrymazaika@gmail.com>
2025-03-23 20:11:35 +0000

    try to run sudo entrypoint but nonsudo format

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -7,7 +7,11 @@ set -eu
 REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 
 echo "## Initializing git repo..."
+git config --global init.defaultBranch main
 git init
+
+echo "## Perms for new git stuff"
+chmod -R 777 .
 
 echo "### Adding git remote..."
 git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
