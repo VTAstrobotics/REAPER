39e8ea8800517dcbfc06e2ac2c852058d779ee84
cameron <ckiw23@gmail.com>
2025-04-22 18:12:12 +0000

    this installs docker, ros2. TODO: isaac ros

--- config/install_dependencies.sh
+++ config/install_dependencies.sh
@@ -33,7 +33,7 @@ sudo apt install -y phoenix6 can-utils
 source /opt/ros/humble/setup.bash
 
 
-ENV PICO_SDK_PATH=$HOME/pico-sdk
+PICO_SDK_PATH=$HOME/pico-sdk
 
 sudo apt install -y \
     gcc-arm-none-eabi \
@@ -59,3 +59,67 @@ pip install numpy opencv-python
 sudo apt-get install -y \
     ros-humble-navigation2 \
     ros-humble-nav2-bringup
+
+# Setup
+## Get initial state with non-root user
+OLDUSR=$(logname)
+OLDHME=$(getent passwd "$OLDUSR" | cut -d: -f6)
+
+## Handle flags
+y_flag=''
+while getopts 'y' flag; do
+        case "${flag}" in
+                y) y_flag='true';;
+                *) exit 1 ;;
+        esac
+done
+
+# Set up Docker's apt repository
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+## Add the repository to sources
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+
+# Install Docker
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Verify installation
+if [[ ! "${y_flag}" ]]; then
+	read -ep "Verify Docker installation? [Y/n] " VRFY
+fi
+if [[ "${y_flag}" || $VRFY == "Y" || $VRFY == "y" || $VRFY == "" ]]; then
+	OLDUSR=$(logname)
+	echo "Verifying Docker installation..."
+	sudo docker run hello-world
+else
+	echo "[WARNING]: Not verifying Docker installation."
+fi
+
+# Post-installation steps
+if [[ ! "${y_flag}" ]]; then
+	read -ep "Want to run docker without sudo? [Y/n] " PSTINSTL
+fi
+if [[ "${y_flag}" || $PSTINSTL == "Y" || $PSTINSTL == "y" || $PSTINSTL == "" ]]; then
+	echo "Beginning post-installation steps"
+	sudo groupadd docker
+	sudo usermod -aG docker $OLDUSR
+	# why not `newgrp docker`? can't `newgrp` in a script for security reasons
+	if [[ $(sg docker -c "docker run hello-world") ]]; then
+		echo "Post-installation steps were successful"
+		echo "Reboot to run docker without sudo"
+	else
+		echo "Post-installation steps were unsuccessful"
+	fi
+else
+	echo "Not performing post-installation steps"
+fi
+
+echo "Docker installed."
