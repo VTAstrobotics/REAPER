2b011efbdc22cf4f114b2672337ecc5edb255fcf
cameron <ckiw23@gmail.com>
2025-05-11 18:02:39 +0000

    added cameras to launch script

--- launch_scripts/launch.sh
+++ launch_scripts/launch.sh
@@ -11,9 +11,9 @@ ros2 run teleop_control Distributor&
 ros2 run dig DigActionServer&
 ros2 run dump DumpActionServer&
 ros2 run drivetrain DriveActionServer&
-ros2 run camera_streamer usbCamStreamer --cam 0 &
-ros2 run camera_streamer usbCamStreamer --cam 2 &
-ros2 run joy joy_node
+ros2 launch camera_streamer camera.launch.py &
+ros2 run joy joy_node &
+ros2 run camera_streamer stream_fuser
 # Teardown
 echo "Shutting down..."
 pkill ros2
@@ -22,4 +22,5 @@ pkill DigActionServer
 pkill DumpActionServer
 pkill DriveActionServer
 pkill usbCamStreamer
+pkill stream_fuser
 echo "Killed background processes"
