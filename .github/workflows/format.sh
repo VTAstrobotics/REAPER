93a53e8fa2b5a0ac7a96991fdb8e81df1bfbfbde
Henry Mazaika <henrymazaika@gmail.com>
2025-05-13 04:19:27 +0000

    UNCOMMENT THE FORMAT SHELL FILE YOU DUMBO

--- .github/workflows/format.sh
+++ .github/workflows/format.sh
@@ -2,25 +2,25 @@
 
 set -e
 
-# echo "### Getting branch"
-# # BRANCH=${GITHUB_REF#*refs/heads/} # this doesnt work on PR, only on push
-# BRANCH=${GITHUB_HEAD_REF} # this doesnt work on push, only on PR
+echo "### Getting branch"
+# BRANCH=${GITHUB_REF#*refs/heads/} # this doesnt work on PR, only on push
+BRANCH=${GITHUB_HEAD_REF} # this doesnt work on push, only on PR
 
-# echo "### Add safe directory"
-# git config --global --add safe.directory /home/astro-dev/src/ci
+echo "### Add safe directory"
+git config --global --add safe.directory /home/astro-dev/src/ci
 
-# echo "### git fetch $BRANCH ..."
-# git fetch origin $BRANCH
+echo "### git fetch $BRANCH ..."
+git fetch origin $BRANCH
 
-# echo "### Branch: $BRANCH (ref: $GITHUB_REF )"
-# git checkout $BRANCH
+echo "### Branch: $BRANCH (ref: $GITHUB_REF )"
+git checkout $BRANCH
 
-# echo "## Configuring git author..."
-# git config --global user.email "clang-format@1337z.ninja"
-# git config --global user.name "Clang Format"
+echo "## Configuring git author..."
+git config --global user.email "clang-format@1337z.ninja"
+git config --global user.name "Clang Format"
 
-# # Ignore workflow files (we may not touch them)
-# git update-index --assume-unchanged .github/workflows/*
+# Ignore workflow files (we may not touch them)
+git update-index --assume-unchanged .github/workflows/*
 
 echo "## Setting up clang-format on C/C++ source"
 SRC_HEADERS=$(git ls-tree --full-tree -r HEAD | grep -e "\.\(h\|hpp\)\$" | cut -f 2)
@@ -40,18 +40,6 @@ find build/ -name compile_commands.json -exec jq -s 'add' {} + > compile_command
 echo "### Source"
 source install/setup.bash
 
-echo "##########################################################################"
-echo "CHECK VERSIONS"
-echo "machine"
-uname -a
-echo "format"
-clang-format --version
-echo "tidy"
-clang-tidy --version
-echo "apply-replacements"
-clang-apply-replacements --version
-echo "##########################################################################"
-
 echo "## Running clang-tidy on C/C++ src code"
 # this script is better than running clang-tidy manually, but has similar effect
 
@@ -68,24 +56,24 @@ python3 .github/workflows/run-clang-tidy.py \
     -style=none \
     src/
 
-# echo "## Run clang-format on C/C++ src code"
-# clang-format -style=file -i $SRC_HEADERS $SRC_IMPLEMENTATION
+echo "## Run clang-format on C/C++ src code"
+clang-format -style=file -i $SRC_HEADERS $SRC_IMPLEMENTATION
 
-# echo "## Commiting files if it still builds"
-# echo "### Clean"
-# rm -rf build/ install/ log/ compile_commands.json
+echo "## Commiting files if it still builds"
+echo "### Clean"
+rm -rf build/ install/ log/ compile_commands.json
 
-# echo "### Build using build script"
-# unset AMENT_PREFIX_PATH
-# source /opt/ros/humble/setup.bash
-# source build_scripts/build.sh 2> >(tee err.log >&2)
+echo "### Build using build script"
+unset AMENT_PREFIX_PATH
+source /opt/ros/humble/setup.bash
+source build_scripts/build.sh 2> >(tee err.log >&2)
 
-# if [ -s err.log ]; then
-#     echo Build failed due to output in stderr. Yes, this fails on warnings. Fix them.;
-#     exit 1;
-# else
-#     # Notice this does not stage any newly created files, bc it creates some junk, so be careful if you choose to change this one day :)
-#     git commit -am "applied C/C++ auto formatting and linting" || true
-#     echo "## Pushing to $BRANCH"
-#     git push -u origin $BRANCH
-# fi
+if [ -s err.log ]; then
+    echo Build failed due to output in stderr. Yes, this fails on warnings. Fix them.;
+    exit 1;
+else
+    # Notice this does not stage any newly created files, bc it creates some junk, so be careful if you choose to change this one day :)
+    git commit -am "applied C/C++ auto formatting and linting" || true
+    echo "## Pushing to $BRANCH"
+    git push -u origin $BRANCH
+fi
